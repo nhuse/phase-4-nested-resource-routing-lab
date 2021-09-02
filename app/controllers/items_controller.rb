231801b54d217fcedbc7be75d5b0f7555a409454
nhuse <nathan.huse@yahoo.com>
2021-09-02 11:39:12 -0500

    commit tree c72898eec58e8dd9002d02500367f40228ec2b17

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,40 @@
 class ItemsController < ApplicationController
 
+  def show
+    item = Item.find_by(id: params[:id])
+    if item
+      render json: item, include: :user
+    else
+      render json: { error: "Item not found" }, status: :not_found
+    end
+  end
+
   def index
-    items = Item.all
+    if params[:user_id]
+      user = User.find_by(id: params[:user_id])
+      if user
+        items = user.items
+      else
+        return render json: { error: "User not found" }, status: :not_found
+      end
+    else
+      items = Item.all
+    end
     render json: items, include: :user
+  end
+
+  def create
+    user = User.find_by(id: params[:user_id])
+    if user
+      item = user.items.create(item_params)
+      render json: item, status: :created
+    end
+  end
+
+  private
+
+  def item_params
+    params.permit(:name, :description, :price, :user_id)
   end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,11 @@ class UsersController < ApplicationController
 
   def show
     user = User.find_by(id: params[:id])
-    render json: user, include: :items
+    if user
+      render json: user, include: :items
+    else
+      render json: { error: "User not found" }, status: :not_found
+    end
   end
 
 end
